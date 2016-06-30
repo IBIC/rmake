b2e89b9522f075c5a0cd00beb0070f5396d1d4b0
Trevor <tkmday@uw.edu>
2016-06-29 17:54:52 -0700

    Can submit jobs with

--- linked-file.mk
+++ linked-file.mk
@@ -4,8 +4,7 @@ sleep: sleep.txt
 
 sleep.txt:
 	lensleep=`shuf -i 1-10 -n 1` ;\
-	echo "sleeping for $$lensleep s" ;\
-	(time sleep $$lensleep) 2> $@
+	echo "slept for $$lensleep""s" > $@
 
 test-%:
 	@echo "$* is '$($*)'"

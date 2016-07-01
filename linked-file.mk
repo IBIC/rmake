8c1b95edd56f845843f3d0f8e1bd19059ea9ed43
Trevor <tkmday@uw.edu>
2016-07-01 11:48:40 -0700

    Submit qsub jobs in a loop, not an array.

--- linked-file.mk
+++ linked-file.mk
@@ -1,10 +1,27 @@
 .PHONY: sleep
+.SECONDARY:
 
-sleep: sleep.txt
+# prevent called libraries from parallelizing
+export OMP_NUM_THREADS=1
+
+FSL_DIR="this is not a directory"
+
+sleep: hexdump.txt 
+
+fslhome: fslhome.txt
 
 sleep.txt:
 	lensleep=`shuf -i 1-10 -n 1` ;\
-	echo "slept for $$lensleep""s" > $@
+	echo "slept for $$lensleep""s" > $@ ;\
+
+hexdump.txt: sleep.txt
+	hd $< >$@
+
+fslhome.txt:
+	echo $(FSL_DIR) > $@
+
+newer: not-orig
+	echo "bar" > newer
 
 test-%:
 	@echo "$* is '$($*)'"
